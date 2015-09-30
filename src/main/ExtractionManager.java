9285f7de0bad116eb3fbd1c29444632c80b5efe2
Marvin Guelzow <marvinguelzow@googlemail.com>
2015-09-30 11:12:40 +0200

    Text instance now ignored bad unicode chars

--- src/main/ExtractionManager.java
+++ src/main/ExtractionManager.java
@@ -7,17 +7,21 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import corpus.CorpusManager;
 import corpus.TextInstance;
 
 public class ExtractionManager {
 	SNExtractor extractor = new SNExtractor();
-	
-	public void processCorpus(String corpusFolder, String resultFolder, int ngramSize)
-	{
+
+	// Set to store all seen ngrams in
+	Set<List<Integer>> uniqueNgrams = new HashSet<List<Integer>>();
+
+	public void processCorpus(String corpusFolder, String resultFolder, int ngramSize) {
 		CorpusManager corpusManager = null;
 		try {
 			corpusManager = new CorpusManager(corpusFolder);
@@ -25,39 +29,47 @@ public class ExtractionManager {
 			System.err.println("Could not initialize corpus manager: " + e.getMessage());
 			System.exit(-1);
 		}
-		
+
 		TextInstance currentTextInstance = corpusManager.getNextText();
 		int textCount = 1;
-		
-		while (currentTextInstance != null)
-		{
+
+		while (currentTextInstance != null) {
 			String currentText = null;
 			try {
-				 currentText = currentTextInstance.getFullText();
+				currentText = currentTextInstance.getFullText();
 			} catch (IOException e) {
-				System.err.println("Could not get text. Skipping " + currentTextInstance.getTextSource());
+				System.err.println("Could not get text. Skipping " + currentTextInstance.getTextSource() + " Cause: " + e.getMessage());
 				currentTextInstance = corpusManager.getNextText();
 				continue;
 			}
-			
+
 			System.out.print("Processing text " + textCount + " of " + corpusManager.getTextCount() + " ... ");
-			processText(currentText, ngramSize, resultFolder, currentTextInstance.getTextSource().getName(), currentTextInstance.getTrueAuthor());
+			processText(currentText, ngramSize, resultFolder, currentTextInstance.getTextSource().getName(),
+					currentTextInstance.getTrueAuthor());
 			System.out.println("Done.");
-			
+
 			currentTextInstance = corpusManager.getNextText();
 			++textCount;
 		}
+
+		ProfileWriter.writeTranslationTable(FeatureMapper.getTranslationTable(), new File(resultFolder));
+		ProfileWriter.writeSeenUniqueNGrams(uniqueNgrams, new File(resultFolder));
 	}
-	
-	private void processText(String text, int ngramSize, String resultFolder, String sourceFile, String author)
-	{
+
+	private void processText(String text, int ngramSize, String resultFolder, String sourceFile, String author) {
 		List<List<String>> ngrams = extractor.extract_SN_Grams(text, ngramSize);
 		List<List<Integer>> numericNGrams = FeatureMapper.numericalizeNGrams(ngrams);
 		HashMap<List<Integer>, Integer> profile = FeatureMapper.createProfile(numericNGrams);
 		writeProfile(profile, resultFolder, sourceFile, author);
+
+		// Store the seen ngrams
+		for (List<Integer> seenNgram : profile.keySet()) {
+			uniqueNgrams.add(seenNgram);
+		}
 	}
-	
-	private void writeProfile(HashMap<List<Integer>, Integer> profile, String resultFolder, String sourceFile, String author) {
+
+	private void writeProfile(HashMap<List<Integer>, Integer> profile, String resultFolder, String sourceFile,
+			String author) {
 		File authorFolder = new File(resultFolder, author);
 		authorFolder.mkdirs();
 		File profileFile = new File(authorFolder, (sourceFile + "_profile"));
@@ -71,10 +83,7 @@ public class ExtractionManager {
 				List<Integer> ngram = entry.getKey();
 				Integer ngramCount = entry.getValue();
 
-				String ngramString = ngram.toString();	
-
-
-
+				String ngramString = ngram.toString();
 
 				writer.write("[" + ngramString.substring(1, ngramString.length() - 1) + "] -> " + ngramCount + "\n");
 			}
@@ -87,9 +96,10 @@ public class ExtractionManager {
 				/* ignore */}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		ExtractionManager ex = new ExtractionManager();
-		ex.processCorpus("Corpus/NEW CORPORA/C10", "Corpus/Processed", 3);
+		// ex.processCorpus("Corpus/NEW CORPORA/C10", "Corpus/Processed", 3);
+		ex.processCorpus("Corpus/NEW CORPORA/pan12A", "Corpus/Processed", 3);
 	}
 }
